bed63fa24629c200c03ffebe5d72e42154504e87
kiittyykat <135552985+kiittyykat@users.noreply.github.com>
2024-01-29 19:19:02 +0200

    Add files via upload

---
+++ hamb.js
@@ -0,0 +1,6 @@
+//hamburger active function
+let hamburger = document.querySelector(".hamburger");
+ hamburger.onclick = function() {
+    let navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
